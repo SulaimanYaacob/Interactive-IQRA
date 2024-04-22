ea224496028bb18d434cd8f4a40bc7b094c10701
Sulaiman Yaacob <sulaimanyaacob99@gmail.com>
2024-04-22 13:14:16 +0800

    added SEO in the root app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { type AppType } from "next/app";
+import Head from "next/head";
 import AppLayout from "~/containers/AppLayout";
 import ClerkAuthProvider from "~/providers/ClerkAuthProvider";
 import MantineStyleProvider from "~/providers/MantineStyleProvider";
@@ -7,13 +8,30 @@ import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkAuthProvider {...pageProps}>
-      <MantineStyleProvider>
-        <AppLayout>
-          <Component {...pageProps} />
-        </AppLayout>
-      </MantineStyleProvider>
-    </ClerkAuthProvider>
+    <>
+      <Head>
+        <title>Interactive Iqra</title>
+        <meta
+          name="description"
+          content="A Real-time online experience for beginners to learn Iqra with friends and tutors or self-taught"
+        />
+        <meta
+          name="keywords"
+          content="Iqra, real-time, online, experience, beginners, self-taught, tutors, friends, learn"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Sulaiman Yaacob" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <p>This is my page content.</p>
+      <ClerkAuthProvider {...pageProps}>
+        <MantineStyleProvider>
+          <AppLayout>
+            <Component {...pageProps} />
+          </AppLayout>
+        </MantineStyleProvider>
+      </ClerkAuthProvider>
+    </>
   );
 };
 
