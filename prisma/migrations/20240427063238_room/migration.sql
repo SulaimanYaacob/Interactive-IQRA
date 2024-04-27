b653ac1a3ec2accffcb38d7d05905513ffbc1d1e
Sulaiman Yaacob <sulaimanyaacob99@gmail.com>
2024-04-27 14:32:59 +0800

    change naming convention for prisma schema

--- prisma/migrations/20240427063238_room/migration.sql
+++ prisma/migrations/20240427063238_room/migration.sql
@@ -1,35 +1,26 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "Role" AS ENUM ('STUDENT', 'TUTOR', 'ADMIN');
 
 -- CreateEnum
 CREATE TYPE "Status" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED', 'CANCELLED');
 
--- DropTable
-DROP TABLE "Post";
-
 -- CreateTable
 CREATE TABLE "Room" (
-    "id" TEXT NOT NULL,
+    "createdByClerkId" TEXT NOT NULL,
     "roomId" TEXT NOT NULL,
     "maxUsers" INTEGER NOT NULL,
-    "identifier" TEXT NOT NULL,
+    "createdByIdentifier" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Room_pkey" PRIMARY KEY ("identifier")
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("createdByIdentifier")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Room_id_key" ON "Room"("id");
+CREATE UNIQUE INDEX "Room_createdByClerkId_key" ON "Room"("createdByClerkId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Room_roomId_key" ON "Room"("roomId");
 
 -- CreateIndex
-CREATE INDEX "Room_id_idx" ON "Room"("id");
+CREATE INDEX "Room_createdByClerkId_idx" ON "Room"("createdByClerkId");
