72ead558b5d9b66827d08aa974dc55e61091dcc8
Sulaiman Yaacob <sulaimanyaacob99@gmail.com>
2024-05-22 23:48:58 +0800

    added regex condition instead of hardcoded word

--- src/utils/iqra-audio.ts
+++ src/utils/iqra-audio.ts
@@ -2,7 +2,7 @@ export function playIqra1Audio(word: string) {
   const path = "/audio/";
   let audio: HTMLAudioElement;
 
-  if (word === "=" || word === "-") return null;
+  if (word.match(/[aeiou=_-]/)) return null;
 
   if (word === "ءَ") audio = new Audio(`${path}أَ.wav`);
   else if (word === "ﻣَ") audio = new Audio(`${path}مَ.wav`);
