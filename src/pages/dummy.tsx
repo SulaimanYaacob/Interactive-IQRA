9019671c89ad9fd6c8108aa5da45b63e3df69991
Sulaiman Yaacob <sulaimanyaacob99@gmail.com>
2024-05-28 20:32:01 +0800

    added types for pbmetada for availability

--- src/pages/dummy.tsx
+++ src/pages/dummy.tsx
@@ -1,9 +1,22 @@
 import { Center, Stack } from "@mantine/core";
+import { DatePicker } from "@mantine/dates";
 
 const DummyPage = () => {
+  console.log(new Date(new Date().getFullYear(), 0, 10));
+  const wed = "0";
+
   return (
     <Stack>
-      <Center w="100%" pos="absolute" mih="100vh" top={0}></Center>
+      <Center w="100%" pos="absolute" mih="100vh" top={0}>
+        <DatePicker
+          getDayProps={(day) => ({
+            style: {
+              background: day.getDay() === 1 ? "red" : "",
+            },
+          })}
+          onChange={(v) => console.log(String(v?.getDay()))}
+        />
+      </Center>
     </Stack>
   );
 };
--- src/types/publicMetadata.ts
+++ src/types/publicMetadata.ts
@@ -1,12 +1,38 @@
 import type { ROLE } from "~/utils/constants";
 
 export interface ClerkPublicMetadata {
-  profileImage: string;
-  bio: string;
   role: ROLE;
+  bio: string;
+  profileImage: string;
+  availability?: Availability;
 }
 
-type availability = {
-  available: boolean;
+type Availability = {
+  mondayAvailability: boolean;
+  mondayStart?: number;
+  mondayEnd?: number;
+  tuesdayAvailability: boolean;
+  tuesdayStart?: number;
+  tuesdayEnd?: number;
+  wednesdayAvailability: boolean;
+  wednesdayStart?: number;
+  wednesdayEnd?: number;
+  thursdayAvailability: boolean;
+  thursdayStart?: number;
+  thursdayEnd?: number;
+  fridayAvailability: boolean;
+  fridayStart?: number;
+  fridayEnd?: number;
+  saturdayAvailability: boolean;
+  saturdayStart?: number;
+  saturdayEnd?: number;
+  sundayAvailability: boolean;
+  sundayStart?: number;
+  sundayEnd?: number;
+};
+
+type Notifications = {
+  id: string;
+  title: string;
   message: string;
 };
